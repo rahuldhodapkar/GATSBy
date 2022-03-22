76a3dec581d00d035213a4f000349c213df8ccff
Rahul Dhodapkar <rahuldhodapkar@Rahuls-MBP.lan>
2022-03-21 22:14:24 -0400

    working toy example

--- src/model/GATSBY.py
+++ src/model/GATSBY.py
@@ -11,20 +11,26 @@ class GATSBY(torch.nn.Module):
         self.in_head = 8
         self.out_head = 1
 
-        self.conv1 = GATConv(dataset.num_features, self.hid, heads=self.in_head, dropout=0.6)
-        self.conv1.__save_att__ = True
-        self.conv2 = GATConv(self.hid*self.in_head, dataset.num_features, concat=False,
-                             heads=self.out_head, dropout=0.6)
-        self.conv2.__save_att__ = True
+        self.conv1 = GATConv(
+            in_channels = dataset.num_features,
+            out_channels = self.hid,
+            heads=self.in_head,
+            dropout=0.6)
+        self.conv2 = GATConv(
+            in_channels = self.hid*self.in_head,
+            out_channels = dataset.num_features,
+            heads=self.out_head,
+            dropout=0.6,
+            concat=False)
+        self.attention = None
 
     def forward(self, data):
-
         x, edge_index = data.x, data.edge_index
         x = F.dropout(x, p=0.6, training=self.training)
         x = self.conv1(x, edge_index)
         x = F.elu(x)
         x = F.dropout(x, p=0.6, training=self.training)
-        x = self.conv2(x, edge_index)
+        x, self.attention = self.conv2(x, edge_index,
+            return_attention_weights=True)
 
-        return F.log_softmax(x, dim=1)
-
+        return x
--- src/model/toy_data_graph_attention.py
+++ src/model/toy_data_graph_attention.py
@@ -25,8 +25,8 @@ edge_index = torch.tensor([[0, 1],
 x = torch.tensor([[5],
                   [3],
                   [1]], dtype=torch.float)
-data = Data(x=x, edge_index=edge_index.t().contiguous())
-data_list = [data]
+data_obj = Data(x=x, edge_index=edge_index.t().contiguous())
+data_list = [data_obj]
 loader = DataLoader(data_list, batch_size=32)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -36,7 +36,7 @@ data = loader.dataset[0].to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=5e-4)
 
 model.train()
-for epoch in range(10):
+for epoch in range(1000):
     model.train()
     optimizer.zero_grad()
     out = model(data)
@@ -46,20 +46,4 @@ for epoch in range(10):
     print("Epoch {:05d} | Loss {:.4f}".format(
         epoch, loss.item()))
 
-
-##### https://github.com/pyg-team/pytorch_geometric/issues/1227
-torch.manual_seed(0)
-l = pyg.nn.GATConv(6,6)
-x = torch.distributions.normal.Normal(0,1).sample((4,6))
-ei = torch.tensor([[1,2,2,0,3,0,1],
-                   [0,0,1,2,2,3,3]],dtype=torch.long)
-
-x, alpha = l(x,ei,return_attention_weights=True)
-
-print(alpha)
-
-
-
-
-
-
+print('All done!')
